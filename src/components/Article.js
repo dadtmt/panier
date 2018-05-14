aa306477d3efe51a27ec4085e09249ee508fe375
tmt <thomas.culdaut@gmail.com>
2018-05-14 10:45:43 +0200

    Article and test

--- src/components/Article.js
+++ src/components/Article.js
@@ -2,6 +2,8 @@
 
 import React from 'react'
 
-const Article = () => <div>Article</div>
+const Article = () => <div>
+    <img alt='benzema' src="https://i0.wp.com/observalgerie.com/wp-content/uploads/2018/05/2170922-45375850-2560-1440.jpg?fit=1050%2C590&ssl=1" />
+</div>
 
 export default Article
--- src/components/Article.test.js
+++ src/components/Article.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Article from './Article';
+import renderer from 'react-test-renderer';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<Article />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders Article html', () => {
+  const tree = renderer
+    .create(<Article />)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
 });
