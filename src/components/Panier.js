e6428294c803f80e1945568d6443da82d16d5232
tmt <thomas.culdaut@gmail.com>
2018-05-15 10:12:26 +0200

    Panier display

--- src/components/Panier.js
+++ src/components/Panier.js
@@ -1,11 +1,45 @@
 // TODO Panier qui affiche le panier
-import React from 'react'
+import React, { Component } from 'react'
 import { Button, Container } from 'reactstrap'
 import ListArticle from './ListArticle'
 
-const Panier = () => <Container>
-    <ListArticle articles={[]}/>
-    <Button />
-</Container>
+class Panier extends Component {
+
+    constructor() {
+        super()
+        //state initial
+        this.state = {
+            articles: [
+                {
+                    name:"Zidane",
+                    price:"45",
+                    imgSrc:"https://www.foot01.com/img/images/650x600/2018/Jan/20/benzema-revient-zidane-recupere-l-arme-fatale-du-real-zidane-16,207169.jpg"
+                },
+                {
+                    name:"Leboeuf",
+                    price:"45",
+                    imgSrc:"https://www.foot01.com/img/images/650x600/2018/Jan/20/benzema-revient-zidane-recupere-l-arme-fatale-du-real-zidane-16,207169.jpg"
+                },
+                {
+                    name:"Pires",
+                    price:"45",
+                    imgSrc:"https://www.foot01.com/img/images/650x600/2018/Jan/20/benzema-revient-zidane-recupere-l-arme-fatale-du-real-zidane-16,207169.jpg"
+                },
+                {
+                    name:"Karembeu",
+                    price:"45",
+                    imgSrc:"https://www.foot01.com/img/images/650x600/2018/Jan/20/benzema-revient-zidane-recupere-l-arme-fatale-du-real-zidane-16,207169.jpg"
+                }
+              ]
+        }
+    }
+
+    render() {
+        return <Container>
+            <ListArticle articles={this.state.articles}/>
+            <Button />
+        </Container>
+    }
+}
 
 export default Panier
